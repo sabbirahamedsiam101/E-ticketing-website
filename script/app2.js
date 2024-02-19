93f45de85de9d99a85aefbee98934b5cdb565b9d
sabbirahamedsiam101 <sabbirahamedsiam101@gmail.com>
2024-02-19 19:27:27 +0600

    add discout section

--- script/app2.js
+++ script/app2.js
@@ -28,8 +28,6 @@ allseats.forEach((seat)=>{
             seat.classList.add('bg-green-500')
             //selectedSeat = selectedSeat + 1 ;
             selectedSeat.push(seat)
-          
-
         }else{
             seat.classList.remove('bg-green-500')
             seat.classList.add('bg-slate-200') 
@@ -42,28 +40,51 @@ allseats.forEach((seat)=>{
         totalseat.innerHTML = currentseat;
         getPricedetails(seat.innerHTML)
         console.log("seat name " + seat.innerHTML);
+        buyseats(seat)
         purchaseSeatnum()
         calculateTotalprice()
-        grandTotalprice()
+        grandTotalprice()    
         applyCupon()
     })
 })
 
+
+// a person can not buy more then 4 seat
+function buyseats(seatName){
+    if(selectedSeat.length > 4){
+       alert('you can not buy more then 4 seat')
+       console.log(seatName)
+       seatName.classList.remove('bg-green-500')
+       seatName.classList.add('bg-slate-200') 
+       selectedSeat.pop();
+    }
+}
+
+// apply the cupon code
 function applyCupon(){
     let inputCupon = document.getElementById('input-cupon');
     let applyButton = document.getElementById('apply');
     let totalPrice  = selectedSeat.length * 550 ;
-   console.log(totalPrice) 
+    console.log(totalPrice) 
     console.log( inputCupon.value)
     console.log( applyButton)
     applyButton.addEventListener('click' , function (){
         if(totalPrice >= 2200 && inputCupon.value === 'NEW15'){
             let discount =  totalPrice * 0.15; 
             let currentPrice = totalPrice - discount
-            console.log(currentPrice);
+            console.log(currentPrice + 'discout');
+            getDiscout(discount) 
             let grandTotal = document.getElementById('grand-total');
             grandTotal.innerHTML = currentPrice;  
-        }else{
+        }else if(totalPrice >= 2200 && inputCupon.value === 'Couple20'){
+            let discount =  totalPrice * 0.20; 
+            let currentPrice = totalPrice - discount
+            console.log(currentPrice + 'discount');
+            getDiscout(discount)
+            let grandTotal = document.getElementById('grand-total');
+            grandTotal.innerHTML = currentPrice; 
+        }
+        else{
             grandTotalprice()
         }
     })
@@ -87,19 +108,28 @@ function getPricedetails(seat) {
     ul.appendChild(newli)
 }
 
+function getDiscout(discount) {
+    let discoutBox = document.getElementById('discout-box');
+    const p1 = document.getElementById('p1');
+    const p2 = document.getElementById('p2');
+    p1.innerHTML = 'Discount';
+    p2.innerHTML = discount;
+}
+// calculate total price
 function calculateTotalprice(){
     let totalPrice = document.getElementById('total-price');
     let currentPrice =  selectedSeat.length * 550
     console.log('currentPrice ' + currentPrice);
     totalPrice.innerHTML = currentPrice
-
 }
 
+// calculate grandtotal price 
 function grandTotalprice(){
     let grandTotal = document.getElementById('grand-total');
     grandTotal.innerHTML = selectedSeat.length * 550;  
 }
 
+// pudate purchase seat number
 function purchaseSeatnum(){
     let purchaseSeat = document.getElementById('purchaseSeat');
     purchaseSeat.innerHTML = selectedSeat.length
